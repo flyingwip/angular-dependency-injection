d02cc196c8f4e46a36f89ed0ee226dc6031d1219
Martijn Wip <flyingwip@gmail.com>
2018-12-23 13:47:23 +0100

    add factory provider for LogDebugger in list component

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,23 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService }  from '../data.service';
 import { OtherDataService }  from '../other-data.service';
+import { LogDebugger } from '../log-debugger.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.sass'],
   providers : [
-  {provide:DataService, useClass:OtherDataService}
+  	DataService,
+    {
+      provide: LogDebugger,
+      useFactory: () => {
+        return new LogDebugger(true);
+      }
+    }
   ]
 })
 export class ListComponent implements OnInit {
   
   items:Array<any>;
 
-  constructor( private dataservice : DataService) { }
+  constructor( private dataservice : DataService, private logDebugger: LogDebugger) { }
 
   ngOnInit() {
-
+  	this.logDebugger.debug('Getting items...');
   	this.items = this.dataservice.getItems();
   }
 
